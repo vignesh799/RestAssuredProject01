2fa85a3799a8208a808fc0b67938e82e5039d675
Vignesh <vignesh.1661shetty@gmail.com>
2026-01-14 17:04:49 +0530

    Code changed in .java class

--- src/test/java/Day01_14_01/HttpRequestsTest.java
+++ src/test/java/Day01_14_01/HttpRequestsTest.java
@@ -6,18 +6,24 @@ package Day01_14_01;
  * then()
  * 
  */
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.annotations.Test;
+
 public class HttpRequestsTest {
 
+	@Test
 	void getusers() {
-	given()
-	
-	.when()
-	
-	.then();
+		given()
+
+				.when().get("https://jsonplaceholder.typicode.com/posts/1")
+				
+				.then()
+				.statusCode(200)
+				.log().all();
 	}
 
 }
