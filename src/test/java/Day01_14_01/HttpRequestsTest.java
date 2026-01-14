9208bb5b60521989291412a60bb590673f29756c
Vignesh <vignesh.1661shetty@gmail.com>
2026-01-14 15:53:51 +0530

    Code changed on 14_01_2026

--- src/test/java/Day01_14_01/HttpRequestsTest.java
+++ src/test/java/Day01_14_01/HttpRequestsTest.java
@@ -1,11 +1,23 @@
 package Day01_14_01;
+
 /*
  * given()
  * when()
  * then()
  * 
  */
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class HttpRequestsTest {
+
+	void getusers() {
+	given()
 	
+	.when()
+	
+	.then();
+	}
 
 }
